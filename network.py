97fb0b952dff924194a59d0396df58efbeb9f0f1
Jack VO <j.vo@linkbynet.com>
2021-12-08 23:02:14 +0700

    add check connectivity

--- network.py
+++ network.py
@@ -8,3 +8,10 @@ def check_localhost():
     if localhost == '127.0.0.1':
         return True
     return False
+
+def check_connectivity():
+    request = requests.get('http://www.google.com')
+    response = request.status_code
+    if response == 200:
+        return True
+    return False
