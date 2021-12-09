cece5e00fa96869fcc1823c4128f9dad51556c68
phuongvo9 <huyphuong4116@gmail.com>
2021-12-09 22:35:46 +0700

    add factorial.py

--- crash-course/loopandrecursion0.py
+++ crash-course/loopandrecursion0.py
@@ -33,3 +33,30 @@ def count_down(start_number):
 #count_down (1)
 
 
+"""
+Find smallest prime factor that is a divisor of x
+"""
+
+def smallest_prime_factor(x):
+    """ Return smallest prime number that is a divisor of x"""
+    # start checking with 2, then move up one by one
+    n = 2
+    while n <=  x:
+        if x % n == 0:
+            return n
+        n += 1
+
+print(smallest_prime_factor(5)) # should be 5
+print(smallest_prime_factor(15)) # should be 3
+print(smallest_prime_factor(19)) # should be 19
+print(smallest_prime_factor(12)) # should be 2
+
+"""
+In math, the factorial of a number is defined as the product of an integer and all the integers below it. For example, the factorial of four (4!) is equal to 1*2*3*4=24.
+Let's make the factorial function return the right number.
+"""
+def factorial(n):
+    result = 1
+    for i in range (1,n+1):
+        result = result * i
+    return result
