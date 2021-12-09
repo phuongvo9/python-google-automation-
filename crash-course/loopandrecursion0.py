f3701d3cc1ab0f70bb43ca07b7d94cdf819cf908
phuongvo9 <huyphuong4116@gmail.com>
2021-12-09 22:11:24 +0700

    add loop-recursion.py

---
+++ crash-course/loopandrecursion0.py
@@ -0,0 +1,15 @@
+#while loops
+
+x = 0
+while x < 5:
+    print ("Not there yet, x=" + str(x))
+    x = x + 1
+
+print ("x= " + str(x))
+
+def attempts (n):
+    x = 1
+    while x <=n:
+        print ("Attempt " +str(x))
+        x += 1
+    print ("Done")
