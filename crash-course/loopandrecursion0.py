8711baa588a52e635767c332c59dc60ea003fdac
phuongvo9 <huyphuong4116@gmail.com>
2021-12-09 22:32:16 +0700

    add smallest prime factor

--- crash-course/loopandrecursion0.py
+++ crash-course/loopandrecursion0.py
@@ -1,11 +1,14 @@
-#while loops
 
-x = 0
-while x < 5:
-    print ("Not there yet, x=" + str(x))
-    x = x + 1
 
-print ("x= " + str(x))
+
+# Back to basics first!
+
+# x = 0
+# while x < 5:
+#     print ("Not there yet, x=" + str(x))
+#     x = x + 1
+
+# print ("x= " + str(x))
 
 def attempts (n):
     x = 1
@@ -13,3 +16,20 @@ def attempts (n):
         print ("Attempt " +str(x))
         x += 1
     print ("Done")
+
+#attempts(9)
+
+
+# ------- Initializing variables matters !! ---
+
+def count_down(start_number):
+    current = start_number
+    while (current > 0):
+        print (str(current))
+        current = current - 1
+    
+    print ("Zero!")
+
+#count_down (1)
+
+
