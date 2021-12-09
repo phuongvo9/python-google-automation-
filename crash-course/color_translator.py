56ec3858b29f31fcd1cd97d73088745b54b47ea7
Jack VO <j.vo@linkbynet.com>
2021-12-09 17:33:34 +0700

    add def color translator

---
+++ crash-course/color_translator.py
@@ -0,0 +1,11 @@
+def color_translator(color):
+    if color == "red":
+        hex_color = "#ff0000"
+    elif color == "green":
+        hex_color = "#00ff00"
+    elif color == "blue":
+        hex_color = "#0000ff"
+    return hex_color
+
+print(color_translator("blue")) #Should be #0000fff
+print(color_translator("yellow")) # should be unknown
