b27a68052e8682ecf373a23f72853ea3972ddb9a
phuongvo9 <huyphuong4116@gmail.com>
2021-12-09 22:37:51 +0700

    add recursion positive number exr

---
+++ crash-course/recursion_sum_positivenumber.py
@@ -0,0 +1,4 @@
+"""
+The function sum_positive_numbers should return the sum of all positive numbers between the number n received and 1. 
+For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15. Let's make this work
+"""
