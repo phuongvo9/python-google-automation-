f63af14dab64c19b7e75dd9fbcceb4e1058f66d8
phuongvo9 <huyphuong4116@gmail.com>
2021-12-10 16:02:19 +0700

    add process_data

--- interact-with-operating-system/exam_generate_report.py
+++ interact-with-operating-system/exam_generate_report.py
@@ -3,6 +3,7 @@
 import csv
 
 def read_employees(csv_file_location):
+    """Return LIST of empleeys from CSV file"""
     csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
     employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
     employee_list = []
@@ -11,3 +12,17 @@ def read_employees(csv_file_location):
     return employee_list
 
 employee_list = read_employees('/home/student-01-2342hh453/data/employees.csv')
+
+
+def process_data (employee_list):
+    department_list = []
+    for employee_data in employee_list:
+        department_list.append(employee_data['Department'])
+
+    department_data = {}
+    for department_name in set(department_list):
+        department_data[department_name] = department_list.count(department_name)
+        return department_data
+
+dictionary = process_data (employee_list)
+
