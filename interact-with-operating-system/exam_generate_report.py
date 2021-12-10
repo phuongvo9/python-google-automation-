39a8da7f62a2d839e572981a85d0214d97e7aa7e
phuongvo9 <huyphuong4116@gmail.com>
2021-12-10 15:51:56 +0700

    add read_employee function v1

---
+++ interact-with-operating-system/exam_generate_report.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import csv
+
+def read_employees(csv_file_location):
+    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+    employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
+    employee_list = []
+    for data in employee_file:
+        employee_list.append(data)
+    return employee_list
+
+employee_list = read_employees('/home/student-01-2342hh453/data/employees.csv')
