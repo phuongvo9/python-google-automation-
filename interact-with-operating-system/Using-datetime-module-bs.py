b050c8fbc6f6d97fa13bd9d5247ec63f4e0c0886
phuongvo9 <huyphuong4116@gmail.com>
2021-12-10 12:56:05 +0700

    OS with Directory

--- interact-with-operating-system/Using-datetime-module-bs.py
+++ interact-with-operating-system/Using-datetime-module-bs.py
@@ -9,3 +9,42 @@ print (datetime.datetime.fromtimestamp(timestamp))
 
 print (os.path.abspath("spider.txt"))
 
+"""Directories"""
+
+#getcwd checks which current directory
+
+print(os.getcwd())
+
+#mkdir function creates a directory
+
+os.mkdir ("new_dir")
+
+# chdir function swtiches to another directory
+
+os.chdir ("new_dir")
+
+print (os.getcwd())
+
+os.chdir("..")
+
+# rmdir fucntion removes a directory
+os.rmdir(os.path.abspath("new_dir"))
+
+os.chdir("..")
+
+# listdir returns a LIST of all files and sub-dir
+
+print(os.listdir("week-two"))
+
+dir = "week-two"
+os.mkdir ("week-two/another_new_dir")
+
+for name in os.listdir(dir):
+    #os .path.join creatse full path and can be used to check if file/directory"
+    fullname = os.path.join(dir,name)
+    if os.path.isdir(fullname):
+        print ("{} is a directory".format(fullname))
+    else:
+        print("{} is a file".format(fullname))
+os.rmdir ("week-two/another_new_dir")
+
