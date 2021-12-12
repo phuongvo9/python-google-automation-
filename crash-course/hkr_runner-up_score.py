1c0dd856671ae5180a33c3f5083b7ed626accd47
phuongvo9 <huyphuong4116@gmail.com>
2021-12-12 20:52:09 +0700

    completed runner up

---
+++ crash-course/hkr_runner-up_score.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+#find the runner-up score. The first line contains . The second line contains an array   of  integers each separated by a space
+#Sample input:
+# 5
+# 2 3 6 6 5
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int,input().split())
+
+scores = list(arr)
+
+
+# winner = 0
+# for i in scores:
+#     if i > winner:
+#         winner = i
+
+winner = max(scores)
+low_scores = []
+for item in scores:
+    if item < winner:
+        low_scores.append(item)
+
+runner_up = max(low_scores)
+print (str(runner_up))
+
+
+
+
+
+
