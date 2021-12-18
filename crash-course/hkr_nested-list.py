c4fda4aa021582933d168ca2b711e98bb16c22f6
phuongvo9 <huyphuong4116@gmail.com>
2021-12-18 11:45:31 +0700

    add nested -list

---
+++ crash-course/hkr_nested-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Given the names and grades for each student in a class of  students,
+store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+"""
+
+if __name__ == '__main__':
+    for _ in range (int(input())):
+        name = input()
+        score = float(input())
+    
