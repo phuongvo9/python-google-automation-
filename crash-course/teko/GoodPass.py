330152fdbefca4b403f5b1fd9961cfe6fa99591e
phuongvo9 <huyphuong4116@gmail.com>
2021-12-18 16:23:55 +0700

    completed

--- crash-course/teko/GoodPass.py
+++ crash-course/teko/GoodPass.py
@@ -9,26 +9,31 @@ def isGoodpassword (yourpass):
     """
     Check password with if else blocks
     """
+    reg = "^(?=.*[a-z])(.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,16}$"
+    # compiling regex
+    pattern = re.compile(reg)
+    is_match = re.search(pattern, yourpass)
+
     # Length is not valid
     if len(yourpass) < 6 or len(yourpass) > 16:
         return False
-    # not have any character in [a-z] - use rawstring from coursera
-    elif not re.match (r"[a-zA-Z0-9$#@]", yourpass):
+    elif not is_match:
         return False
+    # not have any character in [a-z] - use rawstring from coursera
 
-
+    # elif not re.search (r"[a-z]", yourpass):
+    #     return False
     # # not have any character in [A-Z]
-    # elif not re.match (r"[A-Z]", yourpass):
+    # elif not re.search (r"[A-Z]", yourpass):
     #     return False
     # # not have any character in [0-9]
-    # elif not re.match (r"[0-9]", yourpass):
+    # elif not re.search (r"[0-9]", yourpass):
     #     return False
     
-    # elif not re.match (r"[$#@]", yourpass):
+    # elif not re.search (r"[$#@]", yourpass):
     #     return False
 
     return True
-
 
 
 if __name__ == '__main__':
