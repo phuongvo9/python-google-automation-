e95b609ff6ae2c8080674730d83c650bfef98ecc
phuongvohuy9 <huyphuong4116@gmail.com>
2021-12-31 21:43:47 +0700

    add check_web_address.py

---
+++ interact-with-operating-system/regex-regularexpressions/basic-quiz/check_web_address.py
@@ -0,0 +1,6 @@
+"""
+The check_web_address function checks if the text passed qualifies as a top-level web address, \
+meaning that it contains alphanumeric characters (which includes letters, numbers, and underscores), \
+as well as periods, dashes, and a plus sign, followed by a period and a character-only top-level domain such as ".com", ".info", ".edu", etc.
+Write function with the regular expression to do that, using escape characters, wildcards, repetition qualifiers, beginning and end-of-line characters, and character classes.
+"""
