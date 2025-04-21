fbae97aa4b86aa4f829d45c2133d17a6e852b32a
phuongvo9 <huyphuong4116@gmail.com>
2025-04-21 18:52:08 +0700

    feat: implement check_time function for 12-hour clock format validation

---
+++ interact-with-operating-system/3_regex/regex-regularexpressions/basic-quiz/check_time.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+The check_time() function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with no leading zero, followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case
+"""
+
+import re
+def check_time(text):
+  pattern = r'^(1[0-2]|[1-9]):([0-5][0-9]) ?([AaPp][Mm])$'
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
+print(check_time("6:02 am")) # True
+print(check_time("6:02km")) # False
