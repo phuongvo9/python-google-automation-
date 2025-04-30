4bee3ee9a87f14463b4ff516cc97994d64fdafbc
phuongvo9 <huyphuong4116@gmail.com>
2025-04-30 12:06:20 +0700

    feat: add rearrange_name function and unit tests for name formatting

--- interact-with-operating-system/5_unit_tests/example_rearrange/rearrange.py
+++ interact-with-operating-system/5_unit_tests/example_rearrange/rearrange.py
@@ -4,7 +4,10 @@ import re
 """
 This function rearranges a name from "Last, First" format to "First Last" format
 """
-
+# UnboundLocalError: cannot access local variable 'result' where it is not associated with a value
 def rearrange_name(name):
   result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+  if result is None:
+    return name
+
   return "{} {}".format(result[2], result[1])
