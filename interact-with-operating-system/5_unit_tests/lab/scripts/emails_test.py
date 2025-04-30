0770df971c3795ed8efb1611859ee9aef028b5a2
phuongvo9 <huyphuong4116@gmail.com>
2025-04-30 16:00:50 +0700

    feat: add test_one_name test case for find_email function

--- interact-with-operating-system/5_unit_tests/lab/scripts/emails_test.py
+++ interact-with-operating-system/5_unit_tests/lab/scripts/emails_test.py
@@ -7,5 +7,9 @@ class EmailsTest(unittest.TestCase):
     testcase = [None, "Bree", "Campbell"]
     expected = "breee@abc.edu"
     self.assertEqual(find_email(testcase), expected)
+  def test_one_name(self):
+    testcase = [None, "John"]
+    expected = "Missing parameters"
+    self.assertEqual(find_email(testcase), expected)
 if __name__ == '__main__':
   unittest.main()
